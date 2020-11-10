63256b084aafe1e25afb489550a621f73619d7d1
Mark Ewert <mewert03@gmail.com>
2020-11-10 10:39:09 -0800

    Initial sharing of project

---
+++ PPAP.java
@@ -0,0 +1,33 @@
+
+/**
+ * Write a description of class PPAP here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class PPAP
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class PPAP
+     */
+    public PPAP()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
